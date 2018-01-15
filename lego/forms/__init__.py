013b746550e758e82810663b71a529069cf72351
Alex Barbu <alexandru.barbu01@gmail.com>
2018-01-15 16:56:13 +0000

    Added New Team page for admins, implemented penalties for Score Round page and changed one of the missions.

--- lego/forms/__init__.py
+++ lego/forms/__init__.py
@@ -5,6 +5,7 @@
 from lego.forms.login_form import LoginForm
 from lego.forms.score_round_form import ScoreRoundForm
 from lego.forms.edit_team_form import EditTeamForm
+from lego.forms.new_team_form import NewTeamForm
 from lego.forms.edit_team_score_form import EditTeamScoreForm
 from lego.forms.reset_team_score_form import ResetTeamScoreForm
 from lego.forms.stage_form import StageForm

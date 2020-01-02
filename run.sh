a459f99de1f9dc664c9c43316a68f3d5c8abbe88
Harry Tucker <harry.tucker@hpe.com>
2020-01-02 09:38:48 +0000

    Stop run.sh sending all output to /dev/null

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 # Run flask application on any public IP address, on port 5000
 # -----------------------------------------------------------------------------
 
-flask run --host=0.0.0.0 --port=5000 --no-debugger --with-threads --no-reload >/dev/null 2>&1
+flask run --host=0.0.0.0 --port=5000 --no-debugger --with-threads --no-reload

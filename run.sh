f15e531f219382df79ffdb71a43b619c29701052
Harry Tucker <harry.tucker@hpe.com>
2020-01-13 14:05:21 +0000

    Fix run.sh
    Bash scripts are run within a 'sub-shell' so environment variables
    exported in setup.sh do not persist into run.sh. This is now exported
    within run.sh to fix this.

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 # -----------------------------------------------------------------------------
 # Run flask application on any public IP address, on port 5000
 # -----------------------------------------------------------------------------
-
+export FLASK_APP="`pwd`/lego/__init__.py"
 flask run --host=0.0.0.0 --port=5000 --no-debugger --with-threads --no-reload

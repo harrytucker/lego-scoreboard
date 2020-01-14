60d93e1903ead420e55be4bdb7f3abd9db979420
Harry Tucker <harry.tucker@hpe.com>
2020-01-14 11:34:38 +0000

    Fix bug with precision token bonus points
    Also format everything.

--- lego/forms/score_round_form.py
+++ lego/forms/score_round_form.py
@@ -5,7 +5,15 @@
 # -----------------------------------------------------------------------------
 
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, SelectField, IntegerField, StringField, HiddenField, RadioField, Field
+from wtforms import (
+    BooleanField,
+    SelectField,
+    IntegerField,
+    StringField,
+    HiddenField,
+    RadioField,
+    Field,
+)
 from wtforms import Form, FormField
 from wtforms.validators import InputRequired, Optional
 from wtforms.widgets import CheckboxInput
@@ -16,6 +24,7 @@ from collections import OrderedDict
 
 class ScoredFormField(FormField):
     """ extension to the FormField class to allow the form to generate a score based on the fields it contains """
+
     def score(self):
         score = 0
 
@@ -47,7 +56,7 @@ class CheckboxField(Field):
         if self.data:
             return self.value
         else:
-            return '0'
+            return "0"
 
     def process_formdata(self, valuelist):
         super(CheckboxField, self).process_formdata(valuelist)
@@ -58,11 +67,11 @@ class CheckboxField(Field):
 
 
 field_classes = {
-    'BooleanField': BooleanField,
-    'SelectField': SelectField,
-    'StringField': StringField,
-    'RadioField': RadioField,
-    'CheckboxField': CheckboxField
+    "BooleanField": BooleanField,
+    "SelectField": SelectField,
+    "StringField": StringField,
+    "RadioField": RadioField,
+    "CheckboxField": CheckboxField,
 }
 
 
@@ -77,76 +86,92 @@ def parse_json(path):
             # converts the task_no to a string to avoid a crash
             task_no = str(task_no)
 
-            class_ = field_classes[data['type']]
+            class_ = field_classes[data["type"]]
             if class_ in (StringField, BooleanField):
-                mission[task_no] = class_(data['string'])
+                mission[task_no] = class_(data["string"])
             elif class_ == CheckboxField:
-                mission[task_no] = class_(data['string'],
-                                          value=data['value'])
+                mission[task_no] = class_(data["string"], value=data["value"])
             # abandon all hope ye who enter
             # this little manual exemption here sets the default option for this mission as 6
             # as tokens are taken off the field, not added
             # some browsers would pick the first select option (correct) but some would just
             # select the lowest value as default which caused some issues.
-            elif data['string'] == "<span>Number of Precision Tokens left on the Field:</span>":
-                mission[task_no] = class_(data['string'],
-                                          choices=[(choice['value'], choice['string'])
-                                                  for choice in data['choices']],
-                                          default=6,
-                                          validators=[Optional()])
-#                                -"           ^""**$$$e.
-#                              ."                   '$$$c
-#                             /                      "4$$b
-#                            d  3                      $$$$
-#                            $  *                   .$$$$$$
-#                           .$  ^c           $$$$$e$$$$$$$$.
-#                           d$L  4.         4$$$$$$$$$$$$$$b
-#                           $$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
-#               e$""=.      $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
-#              z$$b. ^c     3$$$F "$$$$b   $"$$$$$$$  $$$$*"      .=""$c
-#             4$$$$L        $$P"  "$$b   .$ $$$$$...e$$        .=  e$$$.
-#             ^*$$$$$c  %..   *c    ..    $$ 3$$$$$$$$$$eF     zP  d$$$$$
-#               "**$$$ec   "   %ce""    $$$  $$$$$$$$$$*    .r" =$$$$P""
-#                     "*$b.  "c  *$e.    *** d$$$$$"L$$    .d"  e$$***"
-#                       ^*$$c ^$c $$$      4J$$$$$% $$$ .e*".eeP"
-#                          "$$$$$$"'$=e....$*$$**$cz$$" "..d$*"
-#                            "*$$$  *=%4.$ L L$ P3$$$F $$$P"
-#                               "$   "%*ebJLzb$e$$$$$b $P"
-#                                 %..      4$$$$$$$$$$ "
-#                                  $$$e   z$$$$$$$$$$%
-#                                   "*$c  "$$$$$$$P"
-#                                    ."""*$$$$$$$$bc
-#                                 .-"    .$***$$$"""*e.
-#                              .-"    .e$"     "*$c  ^*b.
-#                       .=*""""    .e$*"          "*bc  "*$e..
-#                     .$"        .z*"               ^*$e.   "*****e.
-#                     $$ee$c   .d"                     "*$.        3.
-#                     ^*$E")$..$"                         *   .ee==d%
-#                        $.d$$$*                           *  J$$$e*
-#                         """""                              "$$$"
+            elif (
+                data["string"]
+                == "<span>Number of Precision Tokens left on the Field:</span>"
+            ):
+                mission[task_no] = class_(
+                    data["string"],
+                    choices=[
+                        (choice["value"], choice["string"])
+                        for choice in data["choices"]
+                    ],
+                    default=6,
+                    validators=[Optional()],
+                )
+            #                                -"           ^""**$$$e.
+            #                              ."                   '$$$c
+            #                             /                      "4$$b
+            #                            d  3                      $$$$
+            #                            $  *                   .$$$$$$
+            #                           .$  ^c           $$$$$e$$$$$$$$.
+            #                           d$L  4.         4$$$$$$$$$$$$$$b
+            #                           $$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
+            #               e$""=.      $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
+            #              z$$b. ^c     3$$$F "$$$$b   $"$$$$$$$  $$$$*"      .=""$c
+            #             4$$$$L        $$P"  "$$b   .$ $$$$$...e$$        .=  e$$$.
+            #             ^*$$$$$c  %..   *c    ..    $$ 3$$$$$$$$$$eF     zP  d$$$$$
+            #               "**$$$ec   "   %ce""    $$$  $$$$$$$$$$*    .r" =$$$$P""
+            #                     "*$b.  "c  *$e.    *** d$$$$$"L$$    .d"  e$$***"
+            #                       ^*$$c ^$c $$$      4J$$$$$% $$$ .e*".eeP"
+            #                          "$$$$$$"'$=e....$*$$**$cz$$" "..d$*"
+            #                            "*$$$  *=%4.$ L L$ P3$$$F $$$P"
+            #                               "$   "%*ebJLzb$e$$$$$b $P"
+            #                                 %..      4$$$$$$$$$$ "
+            #                                  $$$e   z$$$$$$$$$$%
+            #                                   "*$c  "$$$$$$$P"
+            #                                    ."""*$$$$$$$$bc
+            #                                 .-"    .$***$$$"""*e.
+            #                              .-"    .e$"     "*$c  ^*b.
+            #                       .=*""""    .e$*"          "*bc  "*$e..
+            #                     .$"        .z*"               ^*$e.   "*****e.
+            #                     $$ee$c   .d"                     "*$.        3.
+            #                     ^*$E")$..$"                         *   .ee==d%
+            #                        $.d$$$*                           *  J$$$e*
+            #                         """""                              "$$$"
 
             elif class_ in (RadioField, SelectField):
-                mission[task_no] = class_(data['string'],
-                                          choices=[(choice['value'], choice['string'])
-                                                   for choice in data['choices']],
-                                          default=0,
-                                          validators=[Optional()])
+                mission[task_no] = class_(
+                    data["string"],
+                    choices=[
+                        (choice["value"], choice["string"])
+                        for choice in data["choices"]
+                    ],
+                    default=0,
+                    validators=[Optional()],
+                )
             else:
-                raise TypeError('The class with the name {} is not defined in the JSON parser'.format(data['type']))
+                raise TypeError(
+                    "The class with the name {} is not defined in the JSON parser".format(
+                        data["type"]
+                    )
+                )
         missions[key] = ScoredFormField(type(key, (Form,), mission))
 
     # generates FormField containing each missions FormField
-    return FormField(type('Missions', (Form,), missions))
+    return FormField(type("Missions", (Form,), missions))
 
 
 class ScoreRoundForm(FlaskForm):
-    team = SelectField('Team:', validators=[InputRequired(message='Please select a team.')])
-    small_home_zone = BooleanField('Does the robot fit in the small home zone?')
-    yellow_card = BooleanField('Yellow card')
-    confirm = HiddenField(default='0')
-    score = IntegerField('Total score', validators=[Optional()])
+    team = SelectField(
+        "Team:", validators=[InputRequired(message="Please select a team.")]
+    )
+    small_home_zone = BooleanField("Does the robot fit in the small home zone?")
+    yellow_card = BooleanField("Yellow card")
+    confirm = HiddenField(default="0")
+    score = IntegerField("Total score", validators=[Optional()])
 
-    missions = parse_json(os.path.dirname(__file__) + '/../missions.json')
+    missions = parse_json(os.path.dirname(__file__) + "/../missions.json")
 
     def points_scored(self) -> (int, str):
         """Calculate the points scored for this round."""
@@ -155,10 +180,21 @@ class ScoreRoundForm(FlaskForm):
         # protected field '_fields' used due to dynamic generation defining the field names at runtime
         for mission_name, mission in self.missions.form._fields.items():
             score_breakdown[mission_name] = mission.score()
+            print(mission.id)
+            print(mission.score())
+            print(self.small_home_zone.data)
             # successful mission bonus
-            if (mission.id != 'missions-M14 - Precision' and mission.score() > 0 and self.small_home_zone.data is True):
+            if (
+                mission.id != "missions-M14 - Precision"
+                and mission.score() > 0
+                and self.small_home_zone.data is True
+            ):
                 bonus += 5
-            elif (mission.id == 'missions-M02 - Crane (Score All That Apply)' and self.small_home_zone.data is True):
+            if (
+                mission.id == "missions-M02 - Crane (score all that apply)"
+                and mission.score() > 0
+                and self.small_home_zone.data is True
+            ):
                 bonus += 5
 
         # score is the sum of all mission values, with 5 bonus points per successful mission
